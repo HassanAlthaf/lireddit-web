8d8005c6f090a556c61aaabdfc4cbb8e407d78ef
Hassan Althaf <social@hassanalthaf>
2022-05-15 18:08:39 +0530

    feat(NavBar): User-Authenticated State Based Data

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,19 @@
-import { Box, Flex, Link } from "@chakra-ui/react";
+import { Box, Button, Flex, Link } from "@chakra-ui/react";
 import React from "react";
 import NextLink from "next/link";
+import { useMeQuery } from "../generated/graphql";
 
 interface NavBarProps {}
 
 const NavBar: React.FC<NavBarProps> = ({}) => {
-  return (
-    <Flex bg="tomato" p={4} ml={"auto"}>
-      <Box ml={"auto"}>
+  const [{ data, fetching }] = useMeQuery();
+
+  let body = null;
+
+  if (fetching) {
+  } else if (!data?.me) {
+    body = (
+      <>
         <NextLink href="/login" passHref>
           <Link mr={2} color="white">
             Login
@@ -16,7 +22,22 @@ const NavBar: React.FC<NavBarProps> = ({}) => {
         <NextLink href="/register" passHref>
           <Link color="white">Register</Link>
         </NextLink>
-      </Box>
+      </>
+    );
+  } else {
+    body = (
+      <>
+        <Flex color={"white"}>
+          <Box mr={2}>{data.me.username}</Box>
+          <Button variant={"link"}>Logout</Button>
+        </Flex>
+      </>
+    );
+  }
+
+  return (
+    <Flex bg="tomato" p={4} ml={"auto"}>
+      <Box ml={"auto"}>{body}</Box>
     </Flex>
   );
 };
