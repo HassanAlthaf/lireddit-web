396f21c2893783ec6c79a056d7e14637c26ce53f
Hassan Althaf <social@hassanalthaf>
2022-05-16 18:09:13 +0530

    feat(Post): Add field textSnippet + Styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,12 @@
-import { Link } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Link,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
 import { withUrqlClient } from "next-urql";
 import NextLink from "next/link";
 import Layout from "../components/Layout";
@@ -6,22 +14,43 @@ import { usePostsQuery } from "../generated/graphql";
 import { createUrqlClient } from "../utils/createUrqlClient";
 
 const Index = () => {
-  const [{ data }] = usePostsQuery({
+  const [{ data, fetching }] = usePostsQuery({
     variables: {
       limit: 10,
     },
   });
 
+  if (!fetching && !data) {
+    return <div>No posts were found!</div>;
+  }
+
   return (
     <Layout>
-      <NextLink href="/create-post" passHref>
-        <Link>Create Post</Link>
-      </NextLink>
+      <Flex align="center">
+        <Heading>LiReddit</Heading>
+        <NextLink href="/create-post" passHref>
+          <Link ml="auto">Create Post</Link>
+        </NextLink>
+      </Flex>
       <br />
-      {!data?.posts ? (
+      {fetching && !data ? (
         <div>loading...</div>
       ) : (
-        data.posts.map((post) => <div key={post.id}>{post.title}</div>)
+        <Stack spacing={8}>
+          {data?.posts.map((post) => (
+            <Box p={5} key={post.id} shadow={"md"} borderWidth={"1px"}>
+              <Heading fontSize="xl">{post.title}</Heading>
+              <Text mt={4}>{post.textSnippet}</Text>
+            </Box>
+          ))}
+        </Stack>
+      )}
+      {data && (
+        <Flex>
+          <Button m="auto" my={8} isLoading={fetching}>
+            Load more
+          </Button>
+        </Flex>
       )}
     </Layout>
   );
